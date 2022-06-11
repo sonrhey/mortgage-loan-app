c732c9a24374a89fd448fde08be1b3a9d4e12ece
sonrhey27 <sonrhey.deiparine@sun-asterisk.com>
2022-06-11 21:19:39 +0800

    Added Logout

--- resources/views/layouts/template/app.blade.php
+++ resources/views/layouts/template/app.blade.php
@@ -121,10 +121,13 @@
                       <div class="dropdown-divider"></div>
                     </li>
                     <li>
-                      <a class="dropdown-item" href="auth-login-basic.html">
-                        <i class="bx bx-power-off me-2"></i>
-                        <span class="align-middle">Log Out</span>
-                      </a>
+                      <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a class="dropdown-item" href="#" onclick="event.preventDefault(); this.closest('form').submit();">
+                          <i class="bx bx-power-off me-2"></i>
+                          <span class="align-middle">Log Out</span>
+                        </a>
+                      </form>
                     </li>
                   </ul>
                 </li>
